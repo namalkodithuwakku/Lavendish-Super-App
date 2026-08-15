9eb84056eeae959b9534541ac7e3e4c98264b7b0
namalkodithuwakku <namal.kodithuwakku@gmail.com>
2026-08-15 23:01:53 +0530

    Refine Lavendish app header

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,4 +1,12 @@
 'use client';
 import Link from 'next/link';
 import {BellIcon} from './Icons';
-export default function AppHeader(){return <header className="appHeader"><Link href="/" className="brandMini"><img src="/brand/lavendish-logo.jpeg" alt="Lavendish Leisure Hotels & Resorts"/></Link><button className="iconButton" aria-label="Notifications"><BellIcon/><span className="dot"/></button></header>}
+
+export default function AppHeader(){
+  return <header className="appHeader">
+    <Link href="/" className="brandMini" aria-label="Lavendish Leisure home">
+      <span className="logoPlate"><img src="/brand/lavendish-logo.jpeg" alt="Lavendish Leisure Hotels & Resorts"/></span>
+    </Link>
+    <button className="iconButton" aria-label="Notifications"><BellIcon/><span className="dot"/></button>
+  </header>
+}
